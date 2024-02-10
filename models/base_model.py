835a3b2f872768e41179302bd0e4a1ea5817c0c0
JNRBK <rafeekmorgan@yahoo.com>
2024-02-10 22:53:41 +0200

    +x

---
+++ models/base_model.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+from datetime import datetime
+from models import storage
+import uuid
+
+
+class BaseModel():
+    """
+    The BaseModel Class
+    """
+    def __init__(self, *args, **kwargs):
+        """ Initiation of objects. """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                else:
+                    setattr(self, key, value)
+                    if key == 'created_at' or key == 'updated_at':
+                        self.created_at = datetime.fromisoformat(value)
+                        self.updated_at = datetime.fromisoformat(value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
+    def __str__(self):
+        """ Returns the instance representation as string. """
+        class_name = self.__class__.__name__
+        return ("[{}] ({}) {}".format(class_name, self.id, self.__dict__))
+
+    def save(self):
+        """ updates updated_at with the current datetime."""
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """ Returns dict. containing keys/values of __dict__ of instance. """
+        cp_dict = self.__dict__.copy()
+        cp_dict['created_at'] = self.created_at.isoformat()
+        cp_dict['updated_at'] = self.updated_at.isoformat()
+        cp_dict['__class__'] = self.__class__.__name__
+        return cp_dict
