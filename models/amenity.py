78bd442a6ef2a1620e3ddff6f9bd972324cedc0b
Yara-ib <yr.ap.23@gmail.com>
2024-02-10 22:38:45 +0200

    Amenity Module

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Amenity Module """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    The Amenity Class
+
+    Attributes:
+        name: string.
+    """
+    name = ""
