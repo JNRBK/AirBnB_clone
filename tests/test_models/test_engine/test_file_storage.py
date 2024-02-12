89f9f19c4a41eae6811c63ac22d251044d70a25c
Yara-ib <yr.ap.23@gmail.com>
2024-02-13 01:47:51 +0200

    save method testing check

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ FileStorage UnitTest Module"""
+from datetime import datetime, timedelta
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models import storage
@@ -14,6 +15,7 @@ class TestFileStorage(unittest.TestCase):
     def setUpClass(cls):
         """ Creating objects to be tested. """
         cls.f_storage = FileStorage()
+        cls.object = BaseModel()
 
     @classmethod
     def tearDownClass(cls):
@@ -34,6 +36,31 @@ class TestFileStorage(unittest.TestCase):
     def test_objects(self):
         """ Testing the __objects class attribute. """
         self.assertIsInstance(FileStorage.object_get(), dict)
+
+    def test_save(self):
+        """ Testing values after saving. """
+        self.object.save()
+
+        key_name = f'{self.object.__class__.__name__}.{self.object.id}'
+        val = self.f_storage.all()[key_name].updated_at
+
+        # Checking the time is same at time of saving with the saved one
+        t2 = datetime.utcnow()
+        time_difference = abs(val - t2)
+        delta_seconds = time_difference.total_seconds()
+
+        # Making sure BaseModel instance was saved in the storage
+        # By checking the key; classname.id
+        self.assertIn(key_name, self.f_storage.all())
+
+        # Making sure object was saved correctly in storage
+        # having same value of BaseModel instance; eg. updated_at
+        self.assertEqual(self.object.updated_at, val)
+
+        # Checking the time of saving of the object, "almost equal";
+        # Because there's difference in time between creating the object
+        # Then saving it into a file ..
+        self.assertAlmostEqual(val, t2, delta=timedelta(seconds=delta_seconds))
 
 
 if __name__ == '__main__':
