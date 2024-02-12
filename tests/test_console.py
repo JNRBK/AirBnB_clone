aed3638b3dbb18c87abc0da8e538c3a454ead0bb
JNRBK <rafeekmorgan@yahoo.com>
2024-02-12 03:20:29 +0200

    ae

---
+++ tests/test_console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Console UnitTest Module"""
+import cmd
+import unittest
+
+
+class TestConsole(unittest.TestCase):
+    """ Unittest Checking Console """
+
+    @classmethod
+    def setUpClass(cls):
+        """ Creating objects to be tested. """
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        """ Deleting objects at the end of the test"""
+        pass
+
+    def test_check(self):
+        """ Checking the Console commands. """
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
